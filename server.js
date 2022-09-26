faff3484a2d53d89af64308717410acfe9c10fce
ahmedtowsikzap <ahmedtowsikzap@gmail.com>
2022-09-26 19:23:38 +0600

    connection to database

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
 const mongoose = require("mongoose");
 const dotenv = require("dotenv").config();
 const colors = require("colors");
-const DBConnect = require("./utils/dbConnect");
 
 const app = require("./app");
 
 // database connection
-DBConnect();
+mongoose.connect(process.env.DATABASE).then(() => {
+  console.log(`Database conncection successful` .red.bold);
+})
 
 // server
 const port = process.env.PORT || 8080;

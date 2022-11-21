4d0c7acbbdf3503e8f18e753cb376c5d51781bc2
ahmedtowsikzap <ahmedtowsikzap@gmail.com>
2022-11-21 12:45:29 +0600

    code indentation for product route

--- route/product.route.js
+++ route/product.route.js
@@ -3,14 +3,12 @@ const router = express.Router()
 const productController = require('../controllers/product.controller')
 
 
-router.route('/')
-
-.get(productController.getProducts)
-.post(productController.createProduct)
-
-
 router.route('/bulk-update')
 .patch(productController.bulkUpdateProduct)
+
+router.route('/')
+.get(productController.getProducts)
+.post(productController.createProduct)
 
 
 router.route("/:id")
